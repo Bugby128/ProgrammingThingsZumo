b319b43c44f4561165bc08119e3e9f1b5c93745a
Bugby128 <72136172+Bugby128@users.noreply.github.com>
2021-06-17 16:47:07 +0100

    Commented code for task 1 and added a folder for task 2

--- Task1/Arduino Code/Task1/Task1.ino
+++ Task1/Arduino Code/Task1/Task1.ino
@@ -3,12 +3,13 @@
 Zumo32U4Motors motors;
 
 void setup() {
-  Serial1.begin(9600); 
+  Serial1.begin(9600);    //Open serial connection for the XBee
 }
 
 void loop() {
-  int input = Serial1.read(); 
-  switch (input){
+  int input = Serial1.read();   //Read in the value that has been sent over the XBee and store it as a variable
+  
+  switch (input){   //Switch statement that takes the value sent over the XBee and changes the wheel motors speed to move/turn it appropriately 
       case'U':
       motors.setSpeeds(175,175);
       break;
